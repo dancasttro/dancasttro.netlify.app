683e44a33369f0f49f84477aa6aac2a5b4f4d2fe
Daniel Castro <dancasttro@gmail.com>
2020-05-09 00:34:14 -0300

    Updates

---
+++ precache-manifest.c120f71d05a8697af842257ad4bfdcfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd270b99835531e3f9724dc5d6bcdd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb52581130a5b0193228",
+    "url": "/static/js/2.d1743ba7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d1743ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0baa7490fbc76b7c2623",
+    "url": "/static/js/main.e99737c7.chunk.js"
+  },
+  {
+    "revision": "3333d3f772232a8114b4",
+    "url": "/static/js/runtime-main.cf4821a5.js"
+  },
+  {
+    "revision": "229aaa816d3e5df4ca4372d89cf2017b",
+    "url": "/static/media/eu.229aaa81.jpg"
+  },
+  {
+    "revision": "eec4ee6c649ef3c03d3c55b27243d724",
+    "url": "/static/media/hand.eec4ee6c.svg"
+  }
+]);
